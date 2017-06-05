8da1ffd2bf5edc8a298b768f431e6167687d58f6
Chase Pursley <chasepursley@gmail.com>
2017-06-05 18:09:21 -0500

    [SERVER] Change env config.

--- server.js
+++ server.js
@@ -21,6 +21,6 @@ app.post('/message', function(req, res) {
   res.send(200);
 });
 
-var port = process.env.port || 9292;
-server.listen(port);
+var port = process.env.PORT || 9292;
+server.listen(port)
 console.log('Server up and listening on port ' + port);
